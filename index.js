3d5360b0744b5c623718f4c98f1d78cfb9d52a49
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2020-10-02 07:26:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,9 +27,20 @@ let client = op.up;
       }
     )
   );
+
+function makeid(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
   app.use(
     session({
-      secret: config.botcode,
+      secret: makeid(10),
       resave: false,
       saveUninitialized: false
     })
